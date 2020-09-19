df2ac6e1db6754b8b0a233603fcf8a4f1e71f534
ValeedAnjum <allinone644@gmail.com>
2020-09-19 13:02:41 +0500

    use of makeStyles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import { Button } from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import "./App.css";
+const useStyles = makeStyles({
+  thereStyle: {
+    color: "blue",
+    fontSize: "30px",
+  },
+  buttonStyle: {
+    color: "green",
+  },
+});
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <Button color="primary" variant="contained">
+      <Typography color="primary" className={classes.thereStyle}>
+        <h1>Hello Anjum</h1>
+      </Typography>
+      <Button
+        color="primary"
+        className={classes.buttonStyle}
+        variant="contained"
+      >
         This is button component
       </Button>
     </div>
