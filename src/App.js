187357c8db97d9f75c46412b9ee4d42727586026
ValeedAnjum <allinone644@gmail.com>
2020-09-22 09:13:08 +0500

    button component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import { Button, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
 import React from "react";
 import "./App.css";
-const useStyles = makeStyles({
-  thereStyle: {
-    color: "blue",
-    // fontSize: "30px",
-  },
-  buttonStyle: {
-    color: "green",
-  },
-});
+
 function App() {
-  const classes = useStyles();
   return (
     <div className="App">
-      <Typography variant="h1" color="primary" className={classes.thereStyle}>
-        Hello Anjum
-      </Typography>
-      <Button
-        color="primary"
-        className={classes.buttonStyle}
-        variant="contained"
-      >
-        This is button component
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+      <Button variant="contained" color="primary" disableElevation>
+        Disable elevation
       </Button>
     </div>
   );
--- src/theme.js
+++ src/theme.js
@@ -3,9 +3,8 @@ import { createMuiTheme } from "@material-ui/core/styles";
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#ff1a1a",
-      dark: "#080707",
-      contrastText: "yellow",
+      main: "#0DA9DC",
+      dark: "#0396C6",
     },
   },
 });
