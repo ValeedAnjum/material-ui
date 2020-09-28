c037acbc4f8543801985eb17ce29fda113a872cf
ValeedAnjum <allinone644@gmail.com>
2020-09-28 12:06:21 +0500

    appbar 2

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,136 +1,55 @@
+import {
+  AppBar,
+  Badge,
+  IconButton,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
+import MailIcon from "@material-ui/icons/Mail";
 import MenuIcon from "@material-ui/icons/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
-import Button from "@material-ui/core/Button";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { withRouter } from "react-router-dom";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import AccountCircle from "@material-ui/icons/AccountCircle";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
+const useStyles = makeStyles({
   title: {
     flexGrow: 1,
-    [theme.breakpoints.down("xs")]: {
-      flexGrow: 1,
-    },
   },
-  //   headerOptions: {
-  //     display: "flex",
-  //   },
-}));
-
-const Header = (props) => {
-  const { history } = props;
+});
+const Header = () => {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuClick = (pageURL) => {
-    history.push(pageURL);
-    setAnchorEl(null);
-  };
-
-  const handleButtonClick = (pageURL) => {
-    history.push(pageURL);
-  };
-
-  const menuItems = [
-    {
-      menuTitle: "Home",
-      pageURL: "/",
-    },
-    {
-      menuTitle: "Contact",
-      pageURL: "/contact",
-    },
-    {
-      menuTitle: "About",
-      pageURL: "/about",
-    },
-  ];
-
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
+    <div>
+      <AppBar>
         <Toolbar>
+          <IconButton color="inherit" edge="start">
+            <MenuIcon />
+          </IconButton>
           <Typography variant="h6" className={classes.title}>
-            Photos
+            Material-UI
           </Typography>
-          {isMobile ? (
-            <>
-              <IconButton
-                edge="start"
-                className={classes.menuButton}
-                color="inherit"
-                aria-label="menu"
-                onClick={handleMenu}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={open}
-                onClose={() => setAnchorEl(null)}
-              >
-                {menuItems.map((menuItem) => {
-                  const { menuTitle, pageURL } = menuItem;
-                  return (
-                    <MenuItem onClick={() => handleMenuClick(pageURL)}>
-                      {menuTitle}
-                    </MenuItem>
-                  );
-                })}
-              </Menu>
-            </>
-          ) : (
-            <div className={classes.headerOptions}>
-              <Button color="inherit" onClick={() => handleButtonClick("/")}>
-                HOME
-              </Button>
-              <Button
-                color="inherit"
-                onClick={() => handleButtonClick("/contact")}
-              >
-                CONTACT
-              </Button>
-              <Button
-                color="inherit"
-                onClick={() => handleButtonClick("/about")}
-              >
-                ABOUT
-              </Button>
-            </div>
-          )}
+          <div>
+            <IconButton aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={1} color="secondary">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={8} color="secondary">
+                <AccountCircle />
+              </Badge>
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
   );
 };
 
-export default withRouter(Header);
+export default Header;
