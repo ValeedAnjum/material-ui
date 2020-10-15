e6113be814026498eb648f6bd9c8b2f582b13126
ValeedAnjum <allinone644@gmail.com>
2020-10-15 22:58:20 +0500

    applying style to the component childerns using rule names

--- src/Button.js
+++ src/Button.js
@@ -1,31 +1,39 @@
-import React, { Fragment } from "react";
-import { Button } from "@material-ui/core";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
-import clsx from "clsx";
-import { withStyles } from "@material-ui/core/styles";
-
-// We can inject some CSS into the DOM.
-const styles = {
-  root: {
+const useStyles = makeStyles({
+  rootii: {
     background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
     borderRadius: 3,
     border: 0,
     color: "white",
-  },
-  root2: {
     height: 48,
     padding: "0 30px",
     boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
   },
-};
+  label: {
+    textTransform: "uppercase",
+    color: "black",
+  },
+  cp: {
+    color: "white",
+  },
+});
 
-const Buttonc = (props) => {
-  const { classes } = props;
+export default function Buttonc() {
+  const classes = useStyles();
+
   return (
-    <Fragment>
-      <Button className={clsx(classes.root, classes.root2)}>I am Button</Button>
-    </Fragment>
+    <Button
+      color="primary"
+      classes={{
+        root: classes.rootii, // class name, e.g. `classes-nesting-root-x`
+        label: classes.label, // class name, e.g. `classes-nesting-label-x`
+        textPrimary: classes.cp,
+      }}
+    >
+      classes nesting
+    </Button>
   );
-};
-
-export default withStyles(styles)(Buttonc);
+}
