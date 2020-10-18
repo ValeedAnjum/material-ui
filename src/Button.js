f3d0e15c7a5c00ce7763cdd223281547403afbda
ValeedAnjum <allinone644@gmail.com>
2020-10-18 18:32:00 +0500

    Global theme variation

--- src/Button.js
+++ src/Button.js
@@ -1,39 +1,28 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { Fragment } from "react";
+import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 
-const useStyles = makeStyles({
-  rootii: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    borderRadius: 3,
-    border: 0,
-    color: "white",
-    height: 48,
-    padding: "0 30px",
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+const GlobalCss = withStyles({
+  // @global is handled by jss-plugin-global.
+  "@global": {
+    // You should target [class*="MuiButton-root"] instead if you nest themes.
+    ".MuiButton-root": {
+      fontSize: "1rem",
+    },
+    ".MuiButton-textPrimary": {
+      color: "red",
+    },
   },
-  label: {
-    textTransform: "uppercase",
-    color: "black",
-  },
-  cp: {
-    color: "white",
-  },
-});
+})(() => null);
 
 export default function Buttonc() {
-  const classes = useStyles();
-
   return (
-    <Button
-      color="primary"
-      classes={{
-        root: classes.rootii, // class name, e.g. `classes-nesting-root-x`
-        label: classes.label, // class name, e.g. `classes-nesting-label-x`
-        textPrimary: classes.cp,
-      }}
-    >
-      classes nesting
-    </Button>
+    <React.Fragment>
+      <Button color="primary">font-size: 1rem</Button>
+
+      <GlobalCss />
+      <Button color="primary">font-size: 1rem</Button>
+      <Button color="primary">font-size: 2rem</Button>
+    </React.Fragment>
   );
 }
